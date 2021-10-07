2fd6c9f19ccf5e2a046e5c1eefa6551c29f34376
Yahya khodr <yhy97kh18@gmail.com>
2021-10-07 11:20:46 +0300

    first commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+import json
+from django.contrib.auth import authenticate
 # Create your views here.
+
+def login(request):
+    if  request.method== "POST":
+        data =json.loads( request.body)
+        username = data.get("username")
+        password = data.get("password")
+        user = authenticate(request, username=username , password=password)
+    else: 
+        ...
